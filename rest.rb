9e8ff40cabba447fd3a2a255c5c9a26b37de84c7
Arman Ghassemi <arman.gh45@gmail.com>
2014-08-28 10:34:22 -0400

    Update rest.rb

--- rest.rb
+++ rest.rb
@@ -2,21 +2,16 @@ require "rest-client"
 
 
 begin
-
 	puts "Choose an option (1 or 2):"
 	puts "1) Do a google search"
 	puts "2) Ping a heroku app"
-
 rescue
-
 	retry if gets.chomp != '1' || gets.chomp != '2'
-	
 end
 
 choice = gets.chomp
 
 if choice.to_i == 1
-
 	puts "What would you like to seach for?"
 	query = gets.chomp
 	query_lst = query.split(" ")
@@ -32,16 +27,13 @@ if choice.to_i == 1
 			result.concat(s)
 		end
 	end
-
 	return result
 
 else
-
 	puts "Whats the name of your app?"
 	name = gets.chomp.downcase
 	result = RestClient.get '.herokuapp.com'.prepend("#{name}")
 
 	return result
-
 end
 
